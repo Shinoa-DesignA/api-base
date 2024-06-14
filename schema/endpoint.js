5617ac1361439ca604c6d5f42f1536f266d93e53
MannHost <146666179+MannOffc@users.noreply.github.com>
2024-06-14 16:09:35 +0700

    Update endpoint.js

--- schema/endpoint.js
+++ schema/endpoint.js
@@ -46,25 +46,34 @@ const swaggerDocument = {
       description:
         "API endpoints for artificial intelligence content from various platforms.",
     },
-    // {
-    //   name: "Downloader",
-    //   description:
-    //     "API endpoints for downloading content from various platforms.",
-    // },
+    {
+      name: "Cek Resi",
+      description:
+        "API endpoints for check receipts content from various platforms.",
+    },
     // {
     //   name: "Tools",
     //   description: "API endpoints for content tools from multiple platforms.",
     // },
   ],
   paths: {
-    "/api/ai/chatgpt": {
+    "/api/ai/luminai": {
       get: {
         tags: ["AI"],
-        summary: "Chat with GPT AI",
+        summary: "Chat with Lumin AI",
         parameters: [
           {
             in: "query",
             name: "query",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+          {
+            in: "username",
+            name: "username",
             schema: {
               type: "string",
             },
@@ -105,14 +114,14 @@ const swaggerDocument = {
         },
       },
     },
-    "/api/ai/gptlogic": {
+    "/api/ai/gpt4": {
       get: {
         tags: ["AI"],
-        summary: "Chat with GPT Logic",
+        summary: "Chat with GPT4",
         parameters: [
           {
-            in: "query",
-            name: "query",
+            in: "system",
+            name: "system",
             schema: {
               type: "string",
             },
@@ -120,7 +129,16 @@ const swaggerDocument = {
             description: inQuery,
           },
           {
-            in: "query",
+            in: "assistant",
+            name: "assistant",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+          {
+            in: "prompt",
             name: "prompt",
             schema: {
               type: "string",
@@ -163,6 +181,447 @@ const swaggerDocument = {
         },
       },
     },
+    "/api/ai/leptonai": {
+      get: {
+        tags: ["AI"],
+        summary: "Chat with LeptonAi",
+        parameters: [
+          {
+            in: "prompt",
+            name: "prompt",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/ai/letmegpt": {
+      get: {
+        tags: ["AI"],
+        summary: "Chat with LetmeGPT",
+        parameters: [
+          {
+            in: "prompt",
+            name: "prompt",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/ai/thinkany": {
+      get: {
+        tags: ["AI"],
+        summary: "Chat with Thinkany",
+        parameters: [
+          {
+            in: "prompt",
+            name: "prompt",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/ai/isouchat": {
+      get: {
+        tags: ["AI"],
+        summary: "Chat with IsouChat",
+        parameters: [
+          {
+            in: "prompt",
+            name: "prompt",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/ai/yousearch": {
+      get: {
+        tags: ["AI"],
+        summary: "Chat with YouSearch",
+        parameters: [
+          {
+            in: "prompt",
+            name: "prompt",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/ai/aoyo": {
+      get: {
+        tags: ["AI"],
+        summary: "Chat with Aoyo",
+        parameters: [
+          {
+            in: "prompt",
+            name: "prompt",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/ai/llama3": {
+      get: {
+        tags: ["AI"],
+        summary: "Chat with Llama3",
+        parameters: [
+          {
+            in: "prompt",
+            name: "prompt",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/api/ai/gpt4o": {
+      get: {
+        tags: ["AI"],
+        summary: "Chat with GPT4o",
+        parameters: [
+          {
+            in: "prompt",
+            name: "prompt",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    /** "/api/cekresi/jnt": {
+      get: {
+        tags: ["Cek Resi"],
+        summary: "Check JNT Receipt",
+        parameters: [
+          {
+            in: "query",
+            name: "query",
+            schema: {
+              type: "string",
+            },
+            required: true,
+            description: inQuery,
+          },
+        ],
+        responses: {
+          200: {
+            description: "Result successfully returned",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: {
+                    status: {
+                      type: "boolean",
+                      example: true,
+                    },
+                    developer: {
+                      type: "string",
+                      example: config.options.developer,
+                    },
+                    result: {
+                      type: "object",
+                      properties: {
+                        message: {
+                          type: "string",
+                          example:
+                            "Hello! How can I help you with your prompt?",
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    }, **/
   },
   "x-request-time": new Date().toISOString(),
 };
