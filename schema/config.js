1f644dc0bd753f753fe22472c0f3a9ea403dd606
MannHost <146666179+MannOffc@users.noreply.github.com>
2024-06-15 08:26:43 +0700

    Update config.js

--- schema/config.js
+++ schema/config.js
@@ -4,12 +4,11 @@ const options = {
   port: 3000,
   webName: "ManaxuAPIs Playground",
   description: "Free Rest API",
-  favicon:
-    "https://telegra.ph/file/4db35e1d30e8d503918d8.jpg",
+  favicon: "https://telegra.ph/file/4db35e1d30e8d503918d8.jpg",
 };
 
 const host = {
-  BASE_URL: "https://api.manaxu.my.id", // Ganti dengan URL yang sesuai
+  BASE_URL: "https://api-manaxu.vercel.app", // Ganti dengan URL yang sesuai
   // Contoh: https://domain.com
 };
 
