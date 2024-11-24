6c3ee177d146bb902f30bf0f2fdef647648a0808
Sanz-Newbie <134784573+eggspm2@users.noreply.github.com>
2024-11-24 10:29:27 +0700

    config.js

--- schema/config.js
+++ schema/config.js
@@ -1,14 +1,14 @@
 const options = {
-  name: "ManaxuAPIs",
-  developer: "MannR.",
+  name: "Sanz",
+  developer: "Sanzz.",
   port: 3000,
-  webName: "ManaxuAPIs Playground",
+  webName: "Sanz Playground",
   description: "Free Rest API",
   favicon: "https://telegra.ph/file/4db35e1d30e8d503918d8.jpg",
 };
 
 const host = {
-  BASE_URL: "https://api-manaxu.vercel.app", // Ganti dengan URL yang sesuai
+  BASE_URL: "https://api-sanzmd.vercel.app/playground/#/", // Ganti dengan URL yang sesuai
   // Contoh: https://domain.com
 };
 
